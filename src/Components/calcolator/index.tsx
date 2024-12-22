865ca3d488d48f05f11ff48602b26d1a6cbd62bd
hadeelwadia <hadeelwadia@gmail.com>
2024-12-22 16:38:23 +0200

    update on calcolator app

--- src/Components/calcolator/index.tsx
+++ src/Components/calcolator/index.tsx
@@ -4,8 +4,8 @@ import './calcolator.css'
 const drawElements = (elements: string[], handleClick: any): ReactElement =>
   <section className="btns-grid">
     {elements.
-      map((element: string) => <button onClick={() => handleClick(element)} 
-      className={`${element==='+'||element==='-'?'operations-btn':'numbers'}`}>
+      map((element: string) => <button onClick={() => handleClick(element)}
+        className={`${element === '+' || element === '-' ? 'operations-btn' : 'numbers'}`}>
         {element}
       </button>)}
   </section>
@@ -14,11 +14,11 @@ const drawElements = (elements: string[], handleClick: any): ReactElement =>
 
 const Calculator: React.FC = () => {
   const [input, setInput] = useState<string>("");
-  const [equalValue, setEqualValue] = useState<string>("");
+  const [equalValue, setEqualValue] = useState<string>("0");
+  const [haveErrorFormat, setHaveErrorFormat] = useState<boolean>(false);
 
-  const handleClear = () => {
-    setInput("");
-  };
+
+
 
 
   const handleClick = (value: string) => {
@@ -28,22 +28,26 @@ const Calculator: React.FC = () => {
   const handleCalculate = () => {
     try {
       setEqualValue(eval(input).toString());  // Simple calculation using eval
-        setInput('')
+      setInput('')
     } catch (error) {
-      setInput("Error");
+      setInput('')
+      setHaveErrorFormat(true)
+      setEqualValue('0')
+
+
     }
   };
 
   return (
     <main>
-        <section className="cacl-contineer" >
-          <input value={input} className="entered-values"/>
-          <mark className="equal-value" >{equalValue}</mark>
-        </section>
-      {drawElements(['1', '2', '3','4', '5', '6','7', '8', '9','0', '+', '-'],handleClick)}
+      <section className="cacl-contineer" >
+        <input autoFocus value={input} className="entered-values" placeholder={`${haveErrorFormat ? ' Try Agian! ' : 'Start Calcolating!'}`} />
+        <mark className="equal-value" >{equalValue}</mark>
+      </section>
+      {drawElements(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-'], handleClick)}
       <button className="equal-operation" onClick={handleCalculate}>=</button>
     </main>
- 
+
   );
 };
 
